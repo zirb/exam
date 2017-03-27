a98422c292e141f8152bbb7a87de071d18c60a61
zirb <poggi.esiqie@gmail.com>
2017-03-26 19:30:47 -0600

    Initial commit

---
+++ src/main/java/SQLFunctions.java
@@ -0,0 +1,82 @@
+
+import java.sql.*;
+
+/**
+ * Created by Shadow on 3/18/2017.
+ */
+public class SQLFunctions {
+
+    private static final  String URL ="jdbc:mysql://localhost:3306/test?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final  String Username ="root";
+    private static final  String Password ="";
+
+    public static Connection getConnection() throws SQLException {
+
+        Connection con=null;
+
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+
+            con = DriverManager.getConnection(URL,Username,Password);
+
+            Statement statement=con.createStatement();
+            statement.execute("use acceso;");
+            if(!con.isClosed()){
+                System.out.print("Conexion exitosa!!");
+
+            }
+            statement.close();
+
+        }catch(SQLException e){
+            System.out.print(e);
+        }
+        return con;
+    }
+
+    public static Integer insertaAcceso(Connection con,String id,int dentro)
+            throws SQLException {
+        String query = "INSERT INTO accesos (id, fecha, dentro)\n" +
+                "VALUES (?,now(),?);";
+        PreparedStatement stmt = con.prepareStatement(query);
+        int rs=0;
+        try {
+            stmt.setString(1, id);
+            stmt.setInt(2, dentro);
+            rs = stmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.print(e.getSQLState());
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+            con.close();
+            return rs;
+        }
+
+    }
+
+    public static Integer insertaSaldo(Connection con,String id,float saldo)
+            throws SQLException {
+        String query = "UPDATE users  SET saldo=saldo + ? \n" +
+                "WHERE id = ?;";
+        PreparedStatement stmt = con.prepareStatement(query);
+        int rs=0;
+        try {
+            stmt.setFloat(1, saldo);
+            stmt.setString(2, id);
+            rs = stmt.executeUpdate();
+            System.out.println(rs);
+        } catch (SQLException e) {
+            System.out.print(stmt);
+            System.out.println(e.getSQLState());
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+            con.close();
+            return rs;
+        }
+    }
+
+}
---
+++ src/main/java/SerialCon.java
@@ -0,0 +1,131 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Enumeration;
+
+import gnu.io.*;
+import oracle.jrockit.jfr.JFR;
+
+import javax.swing.*;
+
+
+public class SerialCon implements SerialPortEventListener {
+    SerialPort serialPort;
+    /** The port we're normally going to use. */
+    private static final String PORT_NAMES[] = {
+            "/dev/tty.usbserial-A9007UX1", // Mac OS X
+            "/dev/ttyACM0", // Raspberry Pi
+            "/dev/ttyUSB0", // Linux
+            "COM3", // Windows
+    };
+    /**
+     * A BufferedReader which will be fed by a InputStreamReader
+     * converting the bytes into characters
+     * making the displayed results codepage independent
+     */
+    private BufferedReader input;
+    /** The output stream to the port */
+    private OutputStream output;
+    /** Milliseconds to block while waiting for port open */
+    private static final int TIME_OUT = 2000;
+    /** Default bits per second for COM port. */
+    private static final int DATA_RATE = 9600;
+
+    public void initialize() {
+        // the next line is for Raspberry Pi and
+        // gets us into the while loop and was suggested here was suggested http://www.raspberrypi.org/phpBB3/viewtopic.php?f=81&t=32186
+        //System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");
+
+        CommPortIdentifier portId = null;
+        Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+
+        //First, Find an instance of serial port as set in PORT_NAMES.
+        while (portEnum.hasMoreElements()) {
+            CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+            for (String portName : PORT_NAMES) {
+                if (currPortId.getName().equals(portName)) {
+                    portId = currPortId;
+                    break;
+                }
+            }
+        }
+        if (portId == null) {
+            System.out.println("No se pudo encontrar el puerto.");
+            return;
+        }
+
+        try {
+            // open serial port, and use class name for the appName.
+            serialPort = (SerialPort) portId.open(this.getClass().getName(),
+                    TIME_OUT);
+
+            // set port parameters
+            serialPort.setSerialPortParams(DATA_RATE,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+
+            // open the streams
+            input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+            output = serialPort.getOutputStream();
+
+            // add event listeners
+            serialPort.addEventListener(this);
+            serialPort.notifyOnDataAvailable(true);
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
+    }
+
+    /**
+     * This should be called when you stop using the port.
+     * This will prevent port locking on platforms like Linux.
+     */
+    public synchronized void close() {
+        if (serialPort != null) {
+            serialPort.removeEventListener();
+            serialPort.close();
+        }
+    }
+
+    /**
+     * Handle an event on the serial port. Read the data and print it.
+     */
+    public synchronized void serialEvent(SerialPortEvent oEvent) {
+        if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+            try {
+                String inputLine=input.readLine();
+                System.out.println(inputLine);
+
+                if(SQLFunctions.insertaAcceso(SQLFunctions.getConnection(),"wq",1)>0){
+                    System.out.println("Acceso autorizado");
+                }
+            } catch (Exception e) {
+                System.err.println(e.toString());
+                System.out.println("Acceso no autorizado");
+            }
+        }
+        // Ignore all the other eventTypes, but you should consider the other ones.
+    }
+
+    public static void main(String[] args) throws Exception {
+        SerialCon main = new SerialCon();
+        InterfazSaldo ventana= new InterfazSaldo();
+
+        ventana.main(null);
+
+
+        main.initialize();
+        Thread t=new Thread() {
+            public void run() {
+                //the following line will keep this app alive for 1000 seconds,
+                //waiting for events to occur and responding to them (printing incoming messages to console).
+                try {Thread.sleep(1000000);}
+                catch (InterruptedException ie) {}
+            }
+        };
+        t.start();
+        System.out.println("Empezó");
+    }
+}
