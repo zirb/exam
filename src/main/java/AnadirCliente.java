57b815c24db907de55297f7d24f48bda51e26936
zirb <poggi.esiqie@gmail.com>
2017-03-26 21:18:32 -0600

    realiza form de anadir cliente

---
+++ src/main/java/AnadirCliente.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by Shadow on 3/26/2017.
+ */
+public class AnadirCliente {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+}
